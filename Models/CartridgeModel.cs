4ed2ced855d86ef30f9a9aee7319fa8ea587a1f8
Alexey Dmitriev <lesha1599@mail.ru>
2023-04-12 16:22:31 +0300

    Update

--- Models/CartridgeModel.cs
+++ Models/CartridgeModel.cs
@@ -1,20 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AccountingOfCartridges.Data;
 
 namespace AccountingOfCartridges.Models
 {
-    class CartridgeModel
+    class CartridgeModel : Technic
     {
-        public int ID;
-        public string Model;
-        public string Office;
-        public int Status; //Отдан -> Принят на склад -> Передан на заправку/ремонт -> Принят на склад для передачи
-        public string FCs; //ФИО
-        public string BarcodeNumber;
-        public string Printer;
-        public string Comment;
+        public CatridgeData Data;
     }
 }
